072a5467a10dfeeb93261f23d4c33f94fb6229ad
ottodelshammar <ottodelshammar@gmail.com>
2022-12-01 13:57:18 +0100

    quick fix

--- day01/index.js
+++ day01/index.js
@@ -1,21 +1,19 @@
 import fs from 'fs'
 
 const inputDataLinesIntegers = (filename="input.txt") => {
-    return fs.readFileSync(filename).toString().trim().split("\n\n").map(e => e.split("\n").map(e => parseInt(e)))//.map(e => parseInt(e))
+    return fs.readFileSync(filename).toString().trim().split("\n\n").map(e => e.split("\n").map(e => parseInt(e)))
 }
 
 const getSolutionPart1 = () => {
-    const input =inputDataLinesIntegers();
     const totalCaloriesList = [];
-    input.forEach(e => totalCaloriesList.push(e.reduce((a, b) => a + b, 0)));
+    inputDataLinesIntegers().forEach(e => totalCaloriesList.push(e.reduce((a, b) => a + b, 0)));
     const maxCalories = Math.max(...totalCaloriesList)
     return maxCalories
 }
 
 const getSolutionPart2 = () => {
-    const input =inputDataLinesIntegers();
     const totalCaloriesList = [];
-    input.forEach(e => totalCaloriesList.push(e.reduce((a, b) => a + b, 0)));
+    inputDataLinesIntegers().forEach(e => totalCaloriesList.push(e.reduce((a, b) => a + b, 0)));
     totalCaloriesList.sort((a,b) => b-a);
     return totalCaloriesList[0]+totalCaloriesList[1]+totalCaloriesList[2];
 }
