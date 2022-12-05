81cbd7c1544309efea22feabb9ecfa836dee6ab3
ottodelshammar <ottodelshammar@gmail.com>
2022-12-05 11:22:03 +0100

    day05

---
+++ day05/index.js
@@ -0,0 +1,67 @@
+import fs from 'fs'
+
+const inputDataLinesIntegers = (filename = "input.txt") => {
+    return fs.readFileSync(filename).toString().split("\n\n").map(e => e.split("\n").map(e => e.replaceAll("move ", "").replaceAll("from ", "").replaceAll("to ", "").split(" ").map(e => parseInt(e)))).slice(1)[0]
+}
+
+// [T] [V]                     [W]    
+// [V] [C] [P] [D]             [B]    
+// [J] [P] [R] [N] [B]         [Z]    
+// [W] [Q] [D] [M] [T]     [L] [T]    
+// [N] [J] [H] [B] [P] [T] [P] [L]    
+// [R] [D] [F] [P] [R] [P] [R] [S] [G]
+// [M] [W] [J] [R] [V] [B] [J] [C] [S]
+// [S] [B] [B] [F] [H] [C] [B] [N] [L]
+//  1   2   3   4   5   6   7   8   9 
+
+const crates = [
+    ['S', 'M', 'R', 'N', 'W', 'J', 'V', 'T'],
+    ['B', 'W', 'D', 'J', 'Q', 'P', 'C', 'V'],
+    ['B', 'J', 'F', 'H', 'D', 'R', 'P'],
+    ['F', 'R', 'P', 'B', 'M', 'N', 'D'],
+    ['H', 'V', 'R', 'P', 'T', 'B'],
+    ['C', 'B', 'P', 'T'],
+    ['B', 'J', 'R', 'P', 'L'],
+    ['N', 'C', 'S', 'L', 'T', 'Z', 'B', 'W'],
+    ['L', 'S', 'G']
+]
+
+const getTopCrates = (crates) => {
+    const topCrates = [];
+    crates.forEach(crate => topCrates.push(crate.pop()))
+    return topCrates.join('');
+}
+
+const moveCrates = (crates, move) => {
+    for (let m = 0; m < move[0]; m++) {
+        crates[move[2] - 1].push(crates[move[1] - 1].pop())
+    }
+}
+
+const moveMultipleCrates = (crates, move) => {
+    const toMove = [];
+    for(let i = 0; i < move[0]; i++){
+        toMove.push(crates[move[1]-1].pop());
+    }
+    crates[move[2]-1] = crates[move[2]-1].concat(toMove.reverse())  
+}
+
+const getSolutionPart1 = () => {
+    const moves = inputDataLinesIntegers();
+    moves.forEach(move => moveCrates(crates, move))
+    return getTopCrates(crates);
+}
+
+const getSolutionPart2 = () => {
+    const moves = inputDataLinesIntegers();
+    moves.forEach(move => moveMultipleCrates(crates, move))
+    return getTopCrates(crates)
+}
+
+console.log("Javascript")
+const part = process.env.part || "part2"
+
+if (part === "part1")
+    console.log(getSolutionPart1())
+else
+    console.log(getSolutionPart2())
